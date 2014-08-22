8c615f6cb8bbaadc3fbd1a8fa091228ddb4a89f3
金晶 <misi9107@gmail.com>
2014-08-22 15:55:03 +0800

    t1

---
+++ FATS1.3/Areas/Teachings/Models/V_BankDraft.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FATS.Areas.Teachings.Models
+{
+    public class V_BankDraft
+    {
+        public string RemitterName { get; set; }
+        public string RemitterAcc { get; set; }
+        public string RemitterBank { get; set; }
+        public string PayeeName { get; set; }
+        public string PayeeAcc { get; set; }
+        public string PayeeBank { get; set; }
+        public decimal MoneyAmount { get; set; }
+        public DateTime DraftDate { get; set; }
+    }
+}
